83ef1ff6a0cc806743f985a2747714bf210c5dde
Luo <luox3@nci-02181239-ml.fios-router.home>
2020-05-02 21:09:07 -0400

    adding data

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nobel_winners", 
+    version="0.0.1",
+    author="Xing Luo",
+    author_email="xing-luo@outlook.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luox3/project_spring_2020",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+)
